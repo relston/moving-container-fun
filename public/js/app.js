8e87ca7bda5c1b8062e052f21b182641a57b1c34
Ryan Elston <elston.ryan@gmail.com>
2022-04-06 08:03:19 -0700

    Bottom aligned cuboids

--- public/js/app.js
+++ public/js/app.js
@@ -4,9 +4,9 @@ const {
 
 const controller = new GUI()
 const config = {
-    'target-size': 30,
+    'target-size': 50,
+    'rotate-y': 50,
     'rotate-x': 20,
-    'rotate-y': 0,
 }
 const update = () => {
     Object.entries(config).forEach(([key, value]) => {
@@ -15,7 +15,7 @@ const update = () => {
 }
 const sceneFolder = controller.addFolder('Scene')
 sceneFolder.add(config, 'target-size', 20, 100, 1).name('Size').onChange(update)
-sceneFolder.add(config, 'rotate-x', 1, 180, 1).name('Rotate X').onChange(update)
 sceneFolder.add(config, 'rotate-y', 1, 180, 1).name('Rotate Y').onChange(update)
+sceneFolder.add(config, 'rotate-x', 1, 180, 1).name('Rotate X').onChange(update)
 
 update()
