aa2039170a89cb1994ef3ab699395f7585742088
Ryan Elston <elston.ryan@gmail.com>
2022-04-06 08:17:39 -0700

    Vertical controller

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,7 @@ const config = {
     'target-size': 50,
     'rotate-y': 50,
     'rotate-x': 20,
+    'vertical': 30,
 }
 const update = () => {
     Object.entries(config).forEach(([key, value]) => {
@@ -17,5 +18,6 @@ const sceneFolder = controller.addFolder('Scene')
 sceneFolder.add(config, 'target-size', 20, 100, 1).name('Size').onChange(update)
 sceneFolder.add(config, 'rotate-y', 1, 180, 1).name('Rotate Y').onChange(update)
 sceneFolder.add(config, 'rotate-x', 1, 180, 1).name('Rotate X').onChange(update)
+sceneFolder.add(config, 'vertical', 1, 100, 1).name('Vertical').onChange(update)
 
 update()
